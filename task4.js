c706fe477591616f7f4956a5b4680f783104c809
Muhammad Sulthon Imam Puro <75915809+mslthn@users.noreply.github.com>
2025-12-31 13:43:22 +0700

    Add files via upload

--- task4.js
+++ task4.js
@@ -1,44 +1,32 @@
-// Program antri pesan makanan
+// Program antri pesan makanan dengan callback
 const data = [
-    {name : "Andi",
-     wait : 3000
-    },
-    {name : "Budi",
-     wait : 5000
-    },
-    {name : "Citra",
-     wait : 2000
-    },
-    {name : "Dewi",
-     wait : 4000
-    },
-    {name : "Eka",
-     wait : 1000
-    }
-]
+    {name : "Andi", wait : 3000},
+    {name : "Budi", wait : 5000},
+    {name : "Citra", wait : 2000},
+    {name : "Dewi", wait : 4000},
+    {name : "Eka", wait : 1000}
+];
 
-for (let i = 0; i < data.length; i++) {
-        // console.log(data[i].name)
-        setInterval( () => {
-            console.log("menunggu antrian")
-            console.log(data[i].name)
-        }, 1000)
-        
-        setTimeout( () => {
-            console.log(`halo ${data[i].name} pesanan sudah selesai`)
-        }, data[i].wait)
+function processQueue(index, callback) {
+    if (index >= data.length) {
+        callback()
+        return
+    }
+
+    const order = data[index];
+    console.log(`Memproses pesanan untuk ${order.name}...`)
+
+    const waiting = setInterval(() => {
+        console.log(`Menunggu antrian untuk ${order.name}...`)
+    }, 1000)
+
+    setTimeout(() => {
+        clearInterval(waiting)
+        console.log(`Halo ${order.name}, pesanan sudah selesai!`)
+        processQueue(index + 1, callback)
+    }, order.wait)
 }
 
-// function orderFood(data) {
-//     for (i = 0; i < data.length; i++) {
-//         console.log(data[i].name)
-//         // setTimeout( () => {
-//         //     console.log(`halo ${data[i].name} pesanan sudah selesai`)
-//         // }, data[i].wait)
-//     }
-// }
-
-// setInterval( () => {
-//     console.log("menunggu antrian")
-//     console.log(data[i].name)
-// }, 1000)
+processQueue(0, () => {
+    console.log("Semua pesanan telah selesai diproses.")
+})
